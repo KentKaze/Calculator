9c40e7a1448aeabc7793562d272fd1a279043a34
KentKaze <kentnewbie@hotmail.com>
2021-02-10 22:18:53 +0900

    U

--- Calculator/ArithmeticExpression.cpp
+++ Calculator/ArithmeticExpression.cpp
@@ -1,1 +1,23 @@
+#include "Exception.h"
 #include "ArithmeticExpression.h"
+
+void* ArithmeticExpression::GetValue()
+{
+	switch (_op)
+	{
+		/*case OPERATOR_PLUS:
+			return _a->GetValue() + _b->GetValue();
+			break;*/
+	default:
+		throw CA_OPERATOR_IS_INVALID;
+	}
+
+
+}
+
+ArithmeticExpression::ArithmeticExpression(ArithmeticExpression* a, ArithmeticExpression* b, OPERATOR op)
+{
+	_a = a;
+	_b = b;
+	_op = op;
+}
